2050b7f816c3f63ab481f982f9c2081e7d4ec74e
Ilariosenpai <ilanabougit2@gmail.com>
2024-04-04 16:43:15 +0200

    apiadd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './component/header/Header'
 import Weather from './component/weather/Weather'
 import './App.css'
+import axios from 'axios';
 
 function App() {
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+
+    useEffect(() => {
+        axios
+            .get(`https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=Lyon&days=5&aqi=no&alerts=no`)
+            .then(function ({ data }) {
+
+
+                console.log(data);
+            })
+
+            .catch(error => {
+
+                console.log(error);
+            })
+    });
+
+
+
+
+
     return (
 
 
